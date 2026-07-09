f61226ca54ac89ee68ac09f8d887bd2fdb0f583a
Gustavo Volke <gustavo_volke@hotmail.com>
2026-07-09 11:55:49 -0300

    feat: components for the main page

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,6 +5,12 @@ import nextTs from "eslint-config-next/typescript";
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
+  {
+    rules: {
+      "@typescript-eslint/no-empty-object-type": "off",
+      "@typescript-eslint/no-empty-interface": "off",
+    },
+  },
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
---
+++ src/app/(board)/page.tsx
@@ -0,0 +1,54 @@
+import { ArchiveIcon, MessageCircleIcon, ThumbsUpIcon } from "lucide-react";
+import { Section } from "@/components/section";
+import { Card } from "@/components/card";
+
+export default function Home() {
+  return (
+    <div className="max-w-[1620px] w-full mx-auto px-10 flex flex-col h-dvh">
+      <div></div>
+
+      <main className="grid grid-cols-4 gap-5 flex-1 items-stretch">
+        <Section.Root>
+          {/* HEADER */}
+          <Section.Header>
+            <Section.Title>
+              <ArchiveIcon className="size-3" />
+              Backlog
+            </Section.Title>
+
+            <Section.IssueCount>16</Section.IssueCount>
+          </Section.Header>
+
+          {/* CONTEUDO */}
+          <Section.Content>
+            {/* CARDS */}
+            <Card.Root>
+              <Card.Header>
+                <Card.Number>ECO-001</Card.Number>
+                <Card.Title>Implementar cartão de crédito</Card.Title>
+              </Card.Header>
+
+              <Card.Footer>
+                <button
+                  type="button"
+                  className="text-navy-100 flex items-center gap-2 rounded-lg px-2.5 py-1 bg-navy-600 cursor-pointer"
+                >
+                  <ThumbsUpIcon className="size-3" />
+                  <span className="text-sm">12</span>
+                </button>
+
+                <button
+                  type="button"
+                  className="text-navy-100 flex items-center gap-2 rounded-lg px-2.5 py-1 bg-navy-600 cursor-pointer"
+                >
+                  <MessageCircleIcon className="size-3" />
+                  <span className="text-sm">6</span>
+                </button>
+              </Card.Footer>
+            </Card.Root>
+          </Section.Content>
+        </Section.Root>
+      </main>
+    </div>
+  );
+}
